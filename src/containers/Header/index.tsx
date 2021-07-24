58c6801113f88cd2eb59523bbbe21b1595ed757a
RaphaelBRodrigues <raphaelbarbosa.rodrigues@gmail.com>
2021-07-24 17:32:17 -0300

    test: setting tests

--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as S from './styled';
-import { ReactComponent as Logo } from '@Assets/icons/logo.svg';
 import { setCurrentStep } from '@Contexts/NavigationContext';
+import { ReactComponent as Logo } from '@Assets/icons/logo.svg';
 import { useNavigationDispatch } from '@Hooks/useNavigationContext';
 import { Mobile, NotMobile } from '@Components/Responsive';
 import { MenuDesktop, MenuMobile } from '@Components/Menu';
